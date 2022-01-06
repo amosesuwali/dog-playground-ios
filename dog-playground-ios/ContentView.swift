c5e6a44b9f06bec9b48f69ca3c14138d7df9091e
Amose Suwali <amose.suwali@gmail.com>
2022-01-06 21:39:21 +0200

    Ready Set Game
    Ready Set Game

---
+++ dog-playground-ios/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  dog-playground-ios
+//
+//  Created by Amose Munyaradzi on 6/1/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ dog-playground-ios/dog_playground_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  dog_playground_iosApp.swift
+//  dog-playground-ios
+//
+//  Created by Amose Munyaradzi on 6/1/2022.
+//
+
+import SwiftUI
+
+@main
+struct dog_playground_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
